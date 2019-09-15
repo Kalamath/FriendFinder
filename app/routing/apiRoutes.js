30e27eba800cdaec98ccf22b281dd3c4ef6dec2e
Kalamath <thecodedimage@gmail.com>
2019-09-15 13:01:47 -0700

    Routes set up

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,23 @@
 // This is to load the data
 
-var tableData = require("../data/friends");
+var friendsData = require("../data/friends");
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
     // API GET REQUEST
 
-    app.get("api/friends", function(req, res) {
+    app.get("api/friends", function (req, res) {
         res.json(friendsData);
+    });
+    
+    app.post("/api/friends", function (req, res) {
+        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+        // It will do this by sending out the value "true" have a table
+        // req.body is available since we're using the body parsing middleware
+        friends.push(req.body);
+        res.json(true);
     });
 }
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -15,11 +15,12 @@ module.exports = function (app) {
     // ---------------------------------------------------------------------------
 
     // If no matching route is found default to home
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+    
     app.get("*", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
-    app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
-    });
 };
